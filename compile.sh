bce9c289dc0e7e3975308a9260ee15d0be4e6777
AnessFOUKA <foukaaness@gmail.com>
2025-11-27 09:31:32 +0100

    Komodo v1.1.1 : vitashell error 0xF0030000 fixed

--- compile.sh
+++ compile.sh
@@ -6,11 +6,11 @@ echo $i has been compiled !
 done
 arm-vita-eabi-g++ -std=c++17 -Wl,-q -fno-lto -Iinclude/vita src/main.o src/vita/Script.o src/vita/modulesHandler.o src/vita/MemoryManager.o src/vita/GameObject.o src/vita/ItemHandler.o src/vita/Game.o src/vita/InputManager.o src/vita/Engine.o src/vita/AnimatedImage.o src/vita/Camera.o -lvita2d -lSceDisplay_stub -lSceGxm_stub -lSceSysmodule_stub -lSceCtrl_stub -lScePgf_stub -lScePvf_stub -lSceCommonDialog_stub -lfreetype -lpng -ljpeg -lz -lm -lc -lSceAppMgr_stub -o MyApp.elf
 echo elf is created !
-vita-elf-create -s MyApp.elf MyApp.velf
+vita-elf-create MyApp.elf MyApp.velf
 echo velf is created !
 vita-make-fself -s MyApp.velf eboot.bin
 echo eboot is created !
-vita-mksfoex -s TITLE_ID=MYAPP001 "application" param.sfo
+vita-mksfoex -s TITLE_ID=MYAPP0001 "application" param.sfo
 echo param.sfo is created !
 vita-pack-vpk -s param.sfo -a assets=assets -b eboot.bin application.vpk
 echo vpk is created !
--- src/main.cpp
+++ src/main.cpp
@@ -5,16 +5,16 @@ int main(){
         {"cross",SCE_CTRL_CROSS}
     });
     Camera c1({{0,0,200,544}},0,0);
-    g1.getMemoryManager()->addImg("app0:/assets/image.png");
+    g1.getMemoryManager()->addImg("app0:/assets/test.png");
 
     g1.getMemoryManager()->addScript("testb",new Script([&g1,&c1](Script* script){
         if(script->checkCondition(0.0f)){
             script->getMother()->setCameras({&c1});
-            g1.addItem(new AnimatedImage("app0:/assets/image.png",0,0,{{0,0,400,300},{0,0,200,300},{0,0,200,300}},0,1,2,2,{},{}),static_cast<ItemHandler*>(script->getMother()));
+            g1.addItem(new AnimatedImage("app0:/assets/test.png",0,0,{{0,0,400,300},{0,0,200,300},{0,0,200,300}},0,1,2,2,{},{}),static_cast<ItemHandler*>(script->getMother()));
         }
         if(script->checkCondition(1.0f)){
             g1.removeItem(0,static_cast<ItemHandler*>(script->getMother()));
-            g1.getMemoryManager()->removeImg("app0:/assets/image.png");
+            g1.getMemoryManager()->removeImg("app0:/assets/test.png");
         }
     },0.0f,1.0f,1.0f,false));
     g1.getMemoryManager()->getScript("testb")->createCondition(0);

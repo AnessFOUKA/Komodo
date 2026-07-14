b4b1f3467e6c79d4194cf78a2bce3dec9ca0bc3f
AnessFOUKA <foukaaness@gmail.com>
2026-07-14 12:15:58 +0200

    docker-compose creation

--- include/vita/Engine.h
+++
@@ -1,4 +0,0 @@
-#ifndef ENGINE_H
-#define ENGINE_H
-#include "AnimatedImage.h"
-#endif
--- src/sampleClasses/Map.cpp
+++
@@ -1,9 +0,0 @@
-#include "Map.h"
-
-int Map::getScore(){
-    return score;
-}
-
-void Map::setScore(int score){
-    this->score=score;
-}

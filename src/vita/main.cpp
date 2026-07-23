e29c6f79939507e9e4c68465f96799d1996ea094
AnessFOUKA <foukaaness@gmail.com>
2026-07-23 16:20:27 +0200

    file organisation modifications

--- src/vita/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "Komodo.hpp"
-int main(){
-    MemoryManager::storeData("app0:/assets/test.png",TEXTURE);
-    MemoryManager::removeData("app0:/assets/test.png",TEXTURE);
-    Komodo::gameloop();
-    return 0;
-}
